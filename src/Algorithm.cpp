9280da46827068b964cdf7f3770cfc68a130b17b
masaaldosey <prabhatreddyk@gmail.com>
2022-11-23 11:47:16 +0100

    added docstring for the function

--- src/Algorithm.cpp
+++ src/Algorithm.cpp
@@ -1,6 +1,14 @@
 #include "Algorithm.h"
 
+/**
+ * @brief Sum two numbers and return the result.
+ *
+ * @param num_1 First operand.
+ * @param num_2 Second operand.
+ * @return int Result of `num_1` + `num_2`.
+ */
 int AddTwoNumbers(int num_1, int num_2)
 {
-    return num_1 + num_2;
+
+	return num_1 + num_2;
 }
