e42838f3b7e0c1e5dcd6b9f4e4ca77a8a5099005
masaaldosey <prabhatreddyk@gmail.com>
2022-11-24 16:02:12 +0100

    changed function to operate with double type operands

--- src/Algorithm.cpp
+++ src/Algorithm.cpp
@@ -5,9 +5,9 @@
  *
  * @param num_1 First operand.
  * @param num_2 Second operand.
- * @return int Result of `num_1` + `num_2`.
+ * @return double Result of `num_1` + `num_2`.
  */
-int AddTwoNumbers(int num_1, int num_2)
+double AddTwoNumbers(double num_1, double num_2)
 {
 
 	return num_1 + num_2;
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,21 @@
-#include<iostream>
-#include "Algorithm.h"
+#include "cpp-template/Algorithm.h"
+#include <iostream>
 
 int main()
 {
-    int number_1 = 0;
-    int number_2 = 0;
+	double number_1 = 0.0;
+	double number_2 = 0.0;
 
-    std::cout << "THIS IS SIMPLE PROGRAM TO ADD TWO NUMBERS" << std::endl;
+	std::cout << "THIS IS SIMPLE PROGRAM TO ADD TWO NUMBERS" << std::endl;
 
-    std::cout << "Enter the first number : " << std::endl;
-    std::cin >> number_1;
+	std::cout << "Enter the first number : " << std::endl;
+	std::cin >> number_1;
 
-    std::cout << "Enter the second number : " << std::endl;
-    std::cin >> number_2;
+	std::cout << "Enter the second number : " << std::endl;
+	std::cin >> number_2;
 
-    int result = AddTwoNumbers(number_1, number_2);
-    std::cout << number_1 << " + " << number_2 << " = " << result << std::endl;
+	double result = AddTwoNumbers(number_1, number_2);
+	std::cout << number_1 << " + " << number_2 << " = " << result << std::endl;
 
-    return 0;
+	return 0;
 }
