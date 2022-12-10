4b0208bc48b6abbc2393f3239d7b7b6544eef059
masaaldosey <prabhatreddyk@gmail.com>
2022-12-10 18:10:10 +0100

    refactored library and functions. adder is the library and plus_one is the function [skip ci]

---
+++ include/adder/plus_one.hpp
@@ -0,0 +1,28 @@
+/**
+ * @brief One line description of the adder library
+ *
+ * More information about the adder library
+ * can go here.
+ *
+ * @see https://github.com/masaaldoesy/cpp-template
+ */
+#pragma once
+
+namespace adder
+{
+
+/**
+ * @brief One line description of plus_one goes here
+ *
+ * For a more complicated function, here
+ * would be more information about what it does.
+ *
+ * @param x Description of the input parameter x
+ *
+ * @note Something particular to note about plus_one
+ *
+ * @warning A warning about plus_one
+ */
+int plus_one(int x);
+
+} // namespace adder
---
+++ src/plus_one.cpp
@@ -0,0 +1,11 @@
+#include <adder/plus_one.hpp>
+
+namespace adder
+{
+
+int plus_one(int x)
+{
+	return x + 1;
+}
+
+} // namespace adder
